fdc024d3018abe9dc97b343cc9fea6897c184fc5
FinnWoelk <finn.woelk@student.htw-berlin.de>
2024-01-08 20:40:12 +0100

    tests quick safe

--- src/components/__tests__/HelloWorld.spec.ts
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -1,11 +1,23 @@
-import { describe, it, expect } from 'vitest'
+import {describe, it, expect} from 'vitest'
 
-import { mount } from '@vue/test-utils'
+import {mount} from '@vue/test-utils'
 import HelloWorld from '../GameList.vue'
 
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import {createVuetify} from "vuetify";
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
 describe('HelloWorld', () => {
-  it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
-    expect(wrapper.text()).toContain('Hello Vitest')
-  })
+    it('renders properly', () => {
+        const wrapper = mount(HelloWorld,{
+
+            plugins: [vuetify],
+        })
+        expect(wrapper.text()).contain('Create Game')
+    })
 })
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import GameList from "@/components/GameList.vue";
 import GameLobby from "@/components/GameLobby.vue";
 import CreateGameForm from "@/components/CreateGameForm.vue";
+import JoinGame from "@/components/JoinGame.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +14,7 @@ const router = createRouter({
     },
     { path: '/game/create', component: CreateGameForm },
     { path: '/game/:id', component: GameLobby },
+    { path: '/game/:id/join', component: JoinGame },
   ]
 })
 
